9a35eba738e6b7b4fc85813795a1906c2a1d1843
Thomas Broadley <buriedunderbooks@hotmail.com>
2019-03-25 18:16:06 -0400

    Display arrival times next to route descriptions

--- App.js
+++ App.js
@@ -80,67 +80,56 @@ class Weather extends React.Component {
   }
 }
 
-class StopTime extends React.Component {
-  render() {
-    const {
-      stopTime: { tripId, routeNumber, routeDescription, departureTime },
-      criticalTimeHorizon
-    } = this.props;
+const RouteDescription = ({ routeNumber, routeDescription }) =>
+  `${routeNumber}: ${routeDescription}`;
 
-    const timeToDeparture = _.floor(
-      parseDepartureTime(departureTime).diff(getTime(), "seconds") / 60
-    );
-    if (timeToDeparture < 0) return null;
+const TimeToDeparture = ({
+  stopTime: { tripId, routeNumber, routeDescription, departureTime },
+  criticalTimeHorizon
+}) => {
+  const timeToDeparture = _.floor(
+    parseDepartureTime(departureTime).diff(getTime(), "seconds") / 60
+  );
+  if (timeToDeparture < 0) return null;
 
-    return h(
-      Box,
-      {
-        marginBottom: 1,
-        flexDirection: "column",
-        key: tripId
-      },
-      [
-        `${routeNumber}: ${routeDescription}`,
-        h(
-          Color,
-          {
-            bgRed: timeToDeparture <= criticalTimeHorizon
-          },
-          timeToDeparture === 0
-            ? "<1 min"
-            : timeToDeparture === 1
-            ? "1 min"
-            : `${timeToDeparture} mins`
-        )
-      ]
-    );
-  }
-}
+  return h(
+    Color,
+    {
+      bgRed: timeToDeparture <= criticalTimeHorizon
+    },
+    timeToDeparture === 0
+      ? "<1 min"
+      : timeToDeparture === 1
+      ? "1 min"
+      : `${timeToDeparture} mins`
+  );
+};
 
-class BusStop extends React.Component {
-  render() {
-    const {
-      stopName,
-      orderedStopTimes,
-      timeHorizon,
-      criticalTimeHorizon
-    } = this.props;
-    return h(Box, { flexDirection: "column", flexGrow: 1, margin: 1 }, [
-      h(Box, { marginBottom: 1 }, h(Color, { blue: true }, stopName)),
-      ...(orderedStopTimes.length === 0
-        ? [
-            h(
-              Color,
-              { gray: true },
-              `No buses in the next ${timeHorizon} minutes`
+const BusStop = ({
+  stopName,
+  orderedStopTimes,
+  timeHorizon,
+  criticalTimeHorizon
+}) =>
+  h(Box, { flexDirection: "column", flexGrow: 1, margin: 1 }, [
+    h(Box, { marginBottom: 1 }, h(Color, { blue: true }, stopName)),
+    orderedStopTimes.length === 0
+      ? h(Color, { gray: true }, `No buses in the next ${timeHorizon} minutes`)
+      : h(Box, { flexDirection: "row" }, [
+          h(
+            Box,
+            { flexDirection: "column" },
+            orderedStopTimes.map(stopTime => h(RouteDescription, stopTime))
+          ),
+          h(
+            Box,
+            { flexDirection: "column" },
+            orderedStopTimes.map(stopTime =>
+              h(TimeToDeparture, { stopTime, criticalTimeHorizon })
             )
-          ]
-        : orderedStopTimes.map(stopTime =>
-            h(StopTime, { stopTime, criticalTimeHorizon })
-          ))
-    ]);
-  }
-}
+          )
+        ])
+  ]);
 
 class App extends React.Component {
   constructor() {
