e523076cd37b122d00c61591c64263c6dced2575
Thomas Broadley <buriedunderbooks@hotmail.com>
2019-03-25 18:05:20 -0400

    Remove separate BusStop.js file

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-const { Box } = require("ink");
+const { Box, Color } = require("ink");
 const _ = require("lodash");
 const React = require("react");
 const h = require("react-hyperscript");
@@ -10,8 +10,12 @@ const REFRESH_PERIOD = 1;
 
 const OPEN_WEATHER_MAP_CITY_ID = 6176823;
 
-const { BusStop } = require("./BusStop");
-const { getTime, updateDepartureTimes, getCurrentWeather } = require("./lib");
+const {
+  getTime,
+  parseDepartureTime,
+  updateDepartureTimes,
+  getCurrentWeather
+} = require("./lib");
 
 class Clock extends React.Component {
   constructor() {
@@ -73,6 +77,68 @@ class Weather extends React.Component {
     }
 
     return weatherParts.join(", ");
+  }
+}
+
+class StopTime extends React.Component {
+  render() {
+    const {
+      stopTime: { tripId, routeNumber, routeDescription, departureTime },
+      criticalTimeHorizon
+    } = this.props;
+
+    const timeToDeparture = _.floor(
+      parseDepartureTime(departureTime).diff(getTime(), "seconds") / 60
+    );
+    if (timeToDeparture < 0) return null;
+
+    return h(
+      Box,
+      {
+        marginBottom: 1,
+        flexDirection: "column",
+        key: tripId
+      },
+      [
+        `${routeNumber}: ${routeDescription}`,
+        h(
+          Color,
+          {
+            bgRed: timeToDeparture <= criticalTimeHorizon
+          },
+          timeToDeparture === 0
+            ? "<1 min"
+            : timeToDeparture === 1
+            ? "1 min"
+            : `${timeToDeparture} mins`
+        )
+      ]
+    );
+  }
+}
+
+class BusStop extends React.Component {
+  render() {
+    const {
+      stopName,
+      orderedStopTimes,
+      timeHorizon,
+      criticalTimeHorizon
+    } = this.props;
+    return h(Box, { flexDirection: "column", flexGrow: 1, margin: 1 }, [
+      h(Box, { marginBottom: 1 }, h(Color, { blue: true }, stopName)),
+      ...(orderedStopTimes.length === 0
+        ? [
+            h(
+              Color,
+              { gray: true },
+              `No buses in the next ${timeHorizon} minutes`
+            )
+          ]
+        : orderedStopTimes.map(stopTime =>
+            h(StopTime, { stopTime, criticalTimeHorizon })
+          ))
+    ]);
   }
 }
 
